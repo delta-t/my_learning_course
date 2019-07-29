d4de1100175e14192b8f22a95b19004cbace4aca
newman001 <andrey.fedyashev@gmail.com>
2019-07-29 12:56:11 +0500

    even fibonacci numbers

---
+++ even_fibonacci.py
@@ -0,0 +1,22 @@
+"""
+Вывести все четные числа Фибоначчи, которые  меньше заданной величины
+"""
+
+
+def fibonacci():
+    previous_digit, current_digit = 1, 1
+    yield previous_digit
+    while True:
+        yield current_digit
+        previous_digit, current_digit = current_digit, previous_digit + current_digit
+
+
+def even_fibonacci(greatest=10000):
+    for digit in fibonacci():
+        if digit > greatest:
+            return
+        if digit % 2 == 0:
+            yield digit
+
+
+print(list(even_fibonacci()))
