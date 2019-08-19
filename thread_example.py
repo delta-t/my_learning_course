09620b1e49b0e6b8dfef3e19e0acf9dced0ca6ec
newman001 <andrey.fedyashev@gmail.com>
2019-08-19 12:01:39 +0500

    new update

---
+++ thread_example.py
@@ -0,0 +1,39 @@
+import os
+import urllib.request
+from threading import Thread
+
+
+class DownloadThread(Thread):
+    def __init__(self, name, url):
+        Thread.__init__(self)
+        self.name = name
+        self.url = url
+
+    def run(self):
+        handle = urllib.request.urlopen(self.url)
+        fname = os.path.basename(self.url)
+
+        with open(fname, "wb") as f_handler:
+            while True:
+                chunk = handle.read(1024)
+                if not chunk:
+                    break
+                f_handler.write(chunk)
+
+        print("%s закончил загрузку %s!" % (self.name, self.url))
+
+
+def main(urls):
+    for item, url in enumerate(urls):
+        name = "Поток %s" % (item + 1)
+        thread = DownloadThread(url=url, name=name)
+        thread.start()
+
+
+if __name__ == '__main__':
+    urls = ["http://www.irs.gov/pub/irs-pdf/f1040.pdf",
+            "http://www.irs.gov/pub/irs-pdf/f1040a.pdf",
+            "http://www.irs.gov/pub/irs-pdf/f1040ez.pdf",
+            "http://www.irs.gov/pub/irs-pdf/f1040es.pdf",
+            "http://www.irs.gov/pub/irs-pdf/f1040sb.pdf"]
+    main(urls)
