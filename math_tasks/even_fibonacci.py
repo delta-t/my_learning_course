4b77216edd0ce9d973933d1ff8dd4bc9cdb0362a
Andrey <andrey.fedyashev@gmail.com>
2020-05-12 17:44:28 +0500

    update function fibonacci

--- math_tasks/even_fibonacci.py
+++ math_tasks/even_fibonacci.py
@@ -3,21 +3,22 @@
 """
 
 
-def fibonacci():
-    previous_digit, current_digit = 1, 1
-    yield previous_digit
-    while True:
-        yield current_digit
-        previous_digit, current_digit = current_digit, previous_digit + current_digit
+def fibonacci(n: int) -> list:
+    fib = [0, 1] + [0]*(n - 2)
+    for i in range(2, n):
+        fib[i] = fib[i - 1] + fib[i - 2]
+    return fib
 
 
-def even_fibonacci(greatest=10000):
-    for digit in fibonacci():
-        if digit > greatest:
-            return
-        if digit % 2 == 0:
-            yield digit
+def even_fibonacci(greatest: int = 100) -> list:
+    i = 0
+    fibonacci_list = fibonacci(greatest)
+    for number in fibonacci_list:
+        if number % 2 == 0:
+            i = number
+            break
+    return fibonacci_list[i:greatest:3]
 
 
 if __name__ == '__main__':
-    print(list(even_fibonacci()))
+    even_fibonacci()
